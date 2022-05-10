b5bb2525cbb95a035b2b8893127cfe14feba2652
Peppelina <proshekalnikovaa@gmail.com>
2022-05-10 21:25:04 +0300

    add error AddFromNews

--- components/Form/FormAddNews.jsx
+++ components/Form/FormAddNews.jsx
@@ -1,53 +1,77 @@
 import React from 'react';
-import {StyleSheet, TextInput, View, Button, Text, TouchableOpacity} from "react-native";
+import {StyleSheet, TextInput, View, Button, Text, TouchableOpacity, Image} from "react-native";
 import {Formik} from 'formik'
+import * as yup from 'yup'
 
 const FormAddNews = () => {
+    const validationSchema = yup.object().shape(
+        {
+            title: yup.string().typeError('Должно быть строкой').required('Обязательно'),
+            full: yup.string().typeError('Должно быть строкой').required('Обязательно'),
+            data: yup.string().typeError('Должно быть строкой').required('Обязательно'),
+        }
+    )
+
     return (
         <View>
             <Formik initialValues={{
-                title: '', full: '', data: '', img: ''
+                title: '',
+                full: '',
+                data: '',
             }}
+                    validateOnBlur
                     onSubmit={(values, action) => {
                         /*addNews(values) ДОДЕЛАТЬ ПОЛУЧЕНИ ФУНКЦИИ ДОБАЛЕНИЯ НОВОСТИ*/
                         action.resetForm() /*чистка формы*/
-            }}>
-                {(props) => (
+
+                    }}
+                    validationSchema={validationSchema}
+            >
+                {({values, errors, touched,
+                      handleChange, handleBlur,
+                       handleSubmit, }) => (
                     <View>
-                        <TextInput value={props.values.title}
+                        <TextInput type={'text'}
+                                   onChangeText={handleChange('title')}
+                                   onBlur={handleBlur('title')}
+                                   value={values.title}
                                    placeholder={'Введите название новости'}
-                                   onChangeText={props.handleChange('title')}
                                    multiline
                                    style={styles.input}
 
                         />
-                        <TextInput value={props.values.full}
-                                   placeholder={'Введите описание новости'}
-                                   onChangeText={props.handleChange('full')}
-                                   multiline
-                                   style={styles.input}
+                        {touched.title && errors.title && <Text style={styles.error}> {errors.title}</Text>}
+                        <TextInput  type={'text'}
+                                    onChangeText={handleChange('full')}
+                                    onBlur={handleBlur('full')}
+                                    value={values.full}
+                                    placeholder={'Введите описание новости'}
+                                    multiline
+                                    style={styles.input}
 
                         />
-                        <TextInput value={props.values.data}
-                                   placeholder={'Введите дату'}
-                                   onChangeText={props.handleChange('full')}
-                                   multiline
-                                   style={styles.input}
-
+                        {touched.full && errors.full && <Text style={styles.error}> {errors.full}</Text>}
+                        <TextInput  type={'text'}
+                                    onChangeText={handleChange('data')}
+                                    onBlur={handleBlur('data')}
+                                    value={values.data}
+                                    placeholder={'Введите дату'}
+                                    multiline
+                                    style={styles.input}
                         />
-                        <TextInput value={props.values.img}
-                                   placeholder={'Укажите фото'}
-                                   onChangeText={props.handleChange('img')}
-                                   style={styles.input}
+                        {touched.data && errors.data && <Text style={styles.error}> {errors.data}</Text>}
 
-                        />
+                        <Text style={styles.textAdd}> Добавить картинку </Text>
+
+                        <Image style={styles.img} source={require('../../assets/addNews.png')}/>
                         <View style={styles.btn}>
-                            <TouchableOpacity onPress={props.handleSubmit}>
+                            <TouchableOpacity onPress={handleSubmit}>
                                 <Text style={styles.text}> Добавить </Text>
                             </TouchableOpacity>
                         </View>
                     </View>
                 )}
+
             </Formik>
         </View>
     );
@@ -74,6 +98,19 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         color: '#E0FFFF',
         paddingTop: 15
+    },
+    img: {
+        width: 70,
+        height: 70,
+        margin: 10
+    },
+    textAdd: {
+        margin: 10
+    },
+    error: {
+        color: 'red',
+        marginLeft: 10,
+        marginTop: 5
     }
 })
 
