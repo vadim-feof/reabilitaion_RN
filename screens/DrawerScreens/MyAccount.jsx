b80aeed7471d76415cb40d6069940072af9f1441
Peppelina <proshekalnikovaa@gmail.com>
2022-05-14 02:51:32 +0300

    style MyAccount

--- screens/DrawerScreens/MyAccount.jsx
+++ screens/DrawerScreens/MyAccount.jsx
@@ -5,9 +5,18 @@ import {Dimensions, Image, StyleSheet, Text, View} from 'react-native';
 const MyAccount = () => {
     return (
         <View style={styles.container}>
-            <View>
+            <View style={styles.wrapperPhoto}>
                 <Image  style={styles.photo} source={require('../../assets/Angelina.jpg')}/>
+                <Text style={styles.name}> Прощекальникова Геля Дмитриевна</Text>
             </View>
+            <View style={styles.info}>
+                <Text style={styles.item}>Телефон:</Text>
+                <Text style={styles.item}>Почта:</Text>
+                <Text style={styles.item}>Номер карты:</Text>
+                <Text style={styles.item}>День рождения:</Text>
+            </View>
+
+
         </View>
     );
 };
@@ -19,10 +28,33 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'flex-start',
     },
+    name: {
+        marginTop: 15,
+        fontWeight: 'bold',
+        color: '#696969',
+        fontSize: 18
+    },
+    /*info: {
+        flexDirection: 'column',
+        marginTop: 15,
+        display: 'flex',
+        justifyContent: "start" /!*НЕ РАБОТАЕТ*!/
+    },*/
+    item: {
+        marginTop: 7
+    },
     photo: {
         width: 300,
         height: 300,
         borderRadius: Dimensions.get('window').width / 2,
+        marginTop: 20,
+
+    },
+    wrapperPhoto: {
+        width: '100%',
+        height: 400,
+        backgroundColor: '#DCDCDC',
+        alignItems: "center"
     }
 })
 
